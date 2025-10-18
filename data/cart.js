d33b42833f70501e6b6508d66ff1e5c6882426bf
ahmed helalia <ahmedhelalia@gmail.com>
2025-10-18 23:58:23 +0300

    handling date & delivery options

--- data/cart.js
+++ data/cart.js
@@ -2,9 +2,11 @@ export let cart = JSON.parse(localStorage.getItem('cart')) ||
   [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 
 function saveToStorage() {
@@ -39,7 +41,8 @@ export function addToCart(productId) {
   } else {
     cart.push({
       productId: productId,
-      quantity: Number(selectedQuantity)
+      quantity: Number(selectedQuantity),
+      deliveryOptionId: '1'
     })
   }
   saveToStorage();
@@ -76,6 +79,6 @@ export function updateQuantity(productId, newQuantity) {
       matchingItem = cartItem;
     }
   });
-  matchingItem.quantity = newQuantity;  
+  matchingItem.quantity = newQuantity;
   saveToStorage();
 }
