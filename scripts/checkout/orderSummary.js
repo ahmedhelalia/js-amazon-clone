0854ccd81bcb92f560071f32cfe559c25fa41c76
ahmed helalia <ahmedhelalia@gmail.com>
2025-12-17 21:09:21 +0200

    create automated test for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
 import { calculateCartQuantity, cart, removeFromCart, updateQuantity, updateDeliveryOption } from '../../data/cart.js';
 import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import { deliveryOptions, getDeliveryOption,calculateDeliveryDate } from '../../data/delivery-options.js'
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/delivery-options.js'
 import { renderPaymentSummary } from './paymentSummary.js'
 export function renderOrderSummary() {
-  
+
   let cartSummaryHTML = '';
   cart.forEach((cartItem) => {
 
@@ -20,6 +20,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML +=
       `<div class="cart-item-container 
+        js-test-cart-item-container
         js-cart-item-container-${matchingProduct.id}
         ">
               <div class="delivery-date">
@@ -36,7 +37,8 @@ export function renderOrderSummary() {
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity 
+                  js-test-product-quantity-${matchingProduct.id}">
                     <span>
                       Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -45,7 +47,8 @@ export function renderOrderSummary() {
                     </span>
                     <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                     <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}"> Save </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link
+                    js-test-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
---
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,65 @@
+import { loadFromStorage, cart } from "../../data/cart.js";
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+
+describe('test suite: render order summary', () => {
+
+  const productIdOne = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productIdTwo = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  // a hook that will run before each test
+  beforeEach(() => {
+    document.querySelector('.js-test-container').innerHTML = `
+    <div class='js-order-summary'> </div>
+    <div class='js-return-home'> </div>
+    <div class='js-payment-summary'> </div>
+    `;
+
+    spyOn(localStorage, 'setItem')
+    spyOn(localStorage, 'getItem').and.callFake(($item) => {
+      if ($item === 'cart') {
+        return JSON.stringify([{
+          productId: productIdOne,
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: productIdTwo,
+          quantity: 1,
+          deliveryOptionId: '2'
+        }]);
+      } else if ($item === 'cartQuantity') {
+        return " ";
+      } else {
+        return null;
+      }
+    });
+
+    loadFromStorage();
+
+    renderOrderSummary();
+  })
+  // test how the page looks
+  it('displays the cart', () => {
+    expect(document.querySelectorAll('.js-test-cart-item-container').length).toEqual(2);
+
+    expect(document.querySelector(`.js-test-product-quantity-${productIdOne}`).innerText).toContain('Quantity: 2');
+
+    expect(document.querySelector(`.js-test-product-quantity-${productIdTwo}`).innerText).toContain('Quantity: 1');
+  })
+
+  // test how the page behaves
+  it('removes a product', () => {
+    document.querySelector(`.js-test-delete-link-${productIdOne}`).click();
+
+    expect(document.querySelector(`.js-test-product-quantity-${productIdOne}`)).toEqual(null);
+
+    expect(document.querySelector(`.js-test-product-quantity-${productIdTwo}`)).not.toEqual(null);
+
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productIdTwo);
+  })
+
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  })
+
+})
