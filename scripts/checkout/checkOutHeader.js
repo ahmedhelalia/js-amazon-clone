8bf96579ff9fc9dee6efb5e352343669e7789fd4
ahmed helalia <ahmedhelalia@gmail.com>
2025-12-31 16:11:50 +0200

    renaming amazon.html to index.html to support github pages

--- scripts/checkout/checkOutHeader.js
+++ scripts/checkout/checkOutHeader.js
@@ -1,14 +1,14 @@
 export function renderCheckOutHeader() {
   let headerContent = `
   <div class="checkout-header-left-section">
-        <a href="amazon.html">
+        <a href="index.html">
           <img class="amazon-logo" src="images/amazon-logo.png">
           <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
         </a>
       </div>
 
       <div class="checkout-header-middle-section">
-      Checkout (<a class="return-to-home-link js-return-home" href="amazon.html"> 0 items</a>)
+      Checkout (<a class="return-to-home-link js-return-home" href="index.html"> 0 items</a>)
       </div>
 
       <div class="checkout-header-right-section">
